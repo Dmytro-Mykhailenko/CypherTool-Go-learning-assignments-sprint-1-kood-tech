2890158393307e5d2b62e45e299cc7ae5be2736b
dmytromykhailenko <daemonkt@gmail.com>
2024-04-14 17:14:13 +0300

    Updated the code. Implemented error messages. Implemented the sentence reading support

--- cyphertool.go
+++ cyphertool.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -24,8 +27,8 @@ func main() {
 		}
 	case toEncrypt && encoding == "3":
 		{
-			resStr = encrypt_somethingElse(message)
-			encoding = "something else"
+			resStr = encrypt_reverseAlphabetValue(message)
+			encoding = "reverse arphabet value"
 		}
 	case !toEncrypt && encoding == "1":
 		{
@@ -39,8 +42,8 @@ func main() {
 		}
 	case !toEncrypt && encoding == "3":
 		{
-			resStr = decrypt_somethingElse(message)
-			encoding = "something else"
+			resStr = decrypt_reverseAlphabetValue(message)
+			encoding = "reverse alphabet value"
 		}
 	}
 
@@ -56,82 +59,71 @@ func main() {
 
 func getInput() (toEncrypt bool, encoding string, message string) {
 
-	fmt.Println("Select operation (1/2): \n", "1. Encrypt.\n", "2. Decrypt.")
-
-	var operation int
+	var operation string
 	var validOperation bool
 
 	for !validOperation {
-		fmt.Scanln(&operation)
-		if operation == 1 || operation == 2 {
+		fmt.Println("Select operation (1/2): \n", "1. Encrypt.\n", "2. Decrypt.")
+		fmt.Scan(&operation)
+		if operation == "1" || operation == "2" {
 			validOperation = true
+		} else {
+			fmt.Println("\nYou have entered unsuitable selection for operation. Please select again.\n")
 		}
 	}
 
-	if operation == 1 {
+	if operation == "1" {
 		toEncrypt = true
 	}
-	if operation == 2 {
+	if operation == "2" {
 		toEncrypt = false
 	}
-
-	fmt.Println("\nSelect cypher (1/2/3): \n", "1. ROT13.\n", "2. Reverse.\n", "3. Something else.")
 
 	var validEncoding bool
 
 	for !validEncoding {
-		fmt.Scanln(&encoding)
+
+		fmt.Println("\nSelect cypher (1/2/3): \n", "1. ROT13.\n", "2. Reverse.\n", "3. Reverse Alphabet Value.")
+		fmt.Scan(&encoding)
 		if encoding == "1" || encoding == "2" || encoding == "3" {
 			validEncoding = true
+		} else {
+			fmt.Println("\nYou have entered unsuitable selection for operation. Please select again.")
 		}
 	}
 
+	in := bufio.NewReader(os.Stdin)
 	fmt.Println("\nEnter the message: ")
-	fmt.Scanln(&message)
+	text, _ := in.ReadString('\n')
+	text = strings.TrimSpace(text)
+	message += text
 
 	return
 }
 
 // Encrypt the message with rot13
 func encrypt_rot13(s string) string {
-
 	var encryptedStr string
 
-	for x := 0; x < len(s); x++ {
-
-		if s[x] >= 'A' && s[x] <= 'Z' {
-
-			if s[x]+13 > 'Z' {
-
-				encryptedStr += string(s[x] - 13)
-
+	for _, char := range s {
+		if char >= 'A' && char <= 'Z' {
+			if char+13 > 'Z' {
+				encryptedStr += string(char - 13)
 			} else {
-
-				encryptedStr += string(s[x] + 13)
-
+				encryptedStr += string(char + 13)
 			}
-
-		} else if s[x] >= 'a' && s[x] <= 'z' {
-
-			if s[x]+13 > 'z' {
-
-				encryptedStr += string(s[x] - 13)
-
+		} else if char >= 'a' && char <= 'z' {
+			if char+13 > 'z' {
+				encryptedStr += string(char - 13)
 			} else {
-
-				encryptedStr += string(s[x] + 13)
-
+				encryptedStr += string(char + 13)
 			}
-
 		} else {
-
-			encryptedStr += string(s[x])
-
+			encryptedStr += string(char) // Add non-letter characters as they are
 		}
 	}
 
 	return encryptedStr
-
 }
 
 // Encrypt the message with reverse
@@ -160,8 +152,13 @@ func encrypt_reverse(s string) string {
 
 }
 
-func encrypt_somethingElse(s string) string {
-	return "1 + 3 " + s
+func encrypt_reverseAlphabetValue(s string) string {
+	result := ""
+	for i := len(s) - 1; i >= 0; i-- {
+		result += string(s[i])
+	}
+	return result
+
 }
 
 // Decrypt the message with rot13
@@ -232,6 +229,10 @@ func decrypt_reverse(s string) string {
 
 }
 
-func decrypt_somethingElse(s string) string {
-	return "2 + 3 " + s
+func decrypt_reverseAlphabetValue(s string) string {
+	result := ""
+	for i := len(s) - 1; i >= 0; i-- {
+		result += string(s[i])
+	}
+	return result
 }
