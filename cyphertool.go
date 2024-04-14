f97e1660e0f154a1e8e082e86d96c977db73dd54
dmytromykhailenko <daemonkt@gmail.com>
2024-04-14 18:27:19 +0300

    Updated the code. Implemented error messages. Implemented the sentence reading support

--- cyphertool.go
+++ cyphertool.go
@@ -62,13 +62,16 @@ func getInput() (toEncrypt bool, encoding string, message string) {
 	var operation string
 	var validOperation bool
 
+	in := bufio.NewReader(os.Stdin)
+
 	for !validOperation {
 		fmt.Println("Select operation (1/2): \n", "1. Encrypt.\n", "2. Decrypt.")
-		fmt.Scan(&operation)
+		operation, _ = in.ReadString('\n')
+		operation = strings.TrimSpace(operation)
 		if operation == "1" || operation == "2" {
 			validOperation = true
 		} else {
-			fmt.Println("\nYou have entered unsuitable selection for operation. Please select again.\n")
+			fmt.Print("\nYou have entered unsuitable selection for operation. Please select again.\n\n")
 		}
 	}
 
@@ -84,7 +87,8 @@ func getInput() (toEncrypt bool, encoding string, message string) {
 	for !validEncoding {
 
 		fmt.Println("\nSelect cypher (1/2/3): \n", "1. ROT13.\n", "2. Reverse.\n", "3. Reverse Alphabet Value.")
-		fmt.Scan(&encoding)
+		encoding, _ = in.ReadString('\n')
+		encoding = strings.TrimSpace(encoding)
 		if encoding == "1" || encoding == "2" || encoding == "3" {
 			validEncoding = true
 		} else {
@@ -92,37 +96,39 @@ func getInput() (toEncrypt bool, encoding string, message string) {
 		}
 	}
 
-	in := bufio.NewReader(os.Stdin)
-	fmt.Println("\nEnter the message: ")
-	text, _ := in.ReadString('\n')
-	text = strings.TrimSpace(text)
-	message += text
-
+	for message == "" {
+		fmt.Println("\nEnter the message: ")
+		message, _ = in.ReadString('\n')
+		message = strings.TrimSpace(message)
+		if message == "" {
+			fmt.Println("\nYou have entered no message. Please try again.")
+		}
+	}
 	return
 }
 
 // Encrypt the message with rot13
 func encrypt_rot13(s string) string {
+
 	var encryptedStr string
 
-	for _, char := range s {
-		if char >= 'A' && char <= 'Z' {
-			if char+13 > 'Z' {
-				encryptedStr += string(char - 13)
+	for x := 0; x < len(s); x++ {
+		if s[x] >= 'A' && s[x] <= 'Z' {
+			if s[x]+13 > 'Z' {
+				encryptedStr += string(s[x] - 13)
 			} else {
-				encryptedStr += string(char + 13)
+				encryptedStr += string(s[x] + 13)
 			}
-		} else if char >= 'a' && char <= 'z' {
-			if char+13 > 'z' {
-				encryptedStr += string(char - 13)
+		} else if s[x] >= 'a' && s[x] <= 'z' {
+			if s[x]+13 > 'z' {
+				encryptedStr += string(s[x] - 13)
 			} else {
-				encryptedStr += string(char + 13)
+				encryptedStr += string(s[x] + 13)
 			}
 		} else {
-			encryptedStr += string(char) // Add non-letter characters as they are
+			encryptedStr += string(s[x])
 		}
 	}
-
 	return encryptedStr
 }
 
@@ -132,24 +138,15 @@ func encrypt_reverse(s string) string {
 	var encryptedStr string
 
 	for x := 0; x < len(s); x++ {
-
 		if s[x] >= 'A' && s[x] <= 'Z' {
-
 			encryptedStr += string(('Z' - s[x]) + 'A')
-
 		} else if s[x] >= 'a' && s[x] <= 'z' {
-
 			encryptedStr += string(('z' - s[x]) + 'a')
-
 		} else {
-
 			encryptedStr += string(s[x])
-
 		}
 	}
-
 	return encryptedStr
-
 }
 
 func encrypt_reverseAlphabetValue(s string) string {
@@ -167,40 +164,23 @@ func decrypt_rot13(s string) string {
 	var encryptedStr string
 
 	for x := 0; x < len(s); x++ {
-
 		if s[x] >= 'A' && s[x] <= 'Z' {
-
 			if s[x]+13 > 'Z' {
-
 				encryptedStr += string(s[x] - 13)
-
 			} else {
-
 				encryptedStr += string(s[x] + 13)
-
 			}
-
 		} else if s[x] >= 'a' && s[x] <= 'z' {
-
 			if s[x]+13 > 'z' {
-
 				encryptedStr += string(s[x] - 13)
-
 			} else {
-
 				encryptedStr += string(s[x] + 13)
-
 			}
-
 		} else {
-
 			encryptedStr += string(s[x])
-
 		}
 	}
-
 	return encryptedStr
-
 }
 
 // Decrypt the message with reverse
@@ -209,24 +189,15 @@ func decrypt_reverse(s string) string {
 	var encryptedStr string
 
 	for x := 0; x < len(s); x++ {
-
 		if s[x] >= 'A' && s[x] <= 'Z' {
-
 			encryptedStr += string(('Z' - s[x]) + 'A')
-
 		} else if s[x] >= 'a' && s[x] <= 'z' {
-
 			encryptedStr += string(('z' - s[x]) + 'a')
-
 		} else {
-
 			encryptedStr += string(s[x])
-
 		}
 	}
-
 	return encryptedStr
-
 }
 
 func decrypt_reverseAlphabetValue(s string) string {
